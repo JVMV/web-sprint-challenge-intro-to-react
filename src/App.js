a8178cf036f6955c66d71627ff2da081790548f0
Justin Abellera <justin.c.abellera@gmail.com>
2022-07-22 20:22:00 -0700

    first push

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  // the state properties here...
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
